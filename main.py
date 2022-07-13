cce2d7253a09cf325f4a968dd9944abeefe09c1e
Syed Wasif Simnani <wasifsyed@gmail.com>
2022-07-13 11:15:42 +0530

    Changed the file system from txt to json added error exception handlers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 from random import randint,choice,shuffle
 import pyperclip
+import json
 def generate_password():
    
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -28,10 +29,37 @@ def add():
   else: 
     is_ok=messagebox.askokcancel(title="is it ok",message=f"website: {website}\nemail:{email}\npassword: {password}\nis it ok?")
     if is_ok:
-        with open("user_details.txt","a") as file:
-            file.write(f"Website: {website}| email: {email} | password: {password}\n")
-            file.close()
+        new_data={website:{"email":email,"password":password}}
+        try:
+          with open("user_details.json","r") as data_file:
+            data=json.load(data_file)
+        except:
+          with open("user_details.json","w") as data_file:
+            json.dump(new_data,data_file,indent=4)
+        else:
+          data.update(new_data)
+          with open("user_details.json","w") as file:
+              json.dump(data,file,indent=4)
+        finally:
             messagebox.showinfo(title="success",message="Details saved successfully.")
+            website_input.delete(0,END)
+            email_input.delete(0,END)
+            password_input.delete(0,END)
+# ---------------------------- Find Password ------------------------------- #
+def find_password():
+  website=website_input.get()
+  try:
+    with open("user_details.json","r") as data_file:
+         data=json.load(data_file)
+  except:
+      messagebox.showerror(title="file not found error",message="No Data File Found.")
+      return
+  if website in data:
+        email=data[website]["email"]
+        password=data[website]["password"]
+        messagebox.showinfo(title="found successfully",message=f"Your email is {email}\n Your password is {password}\n")
+  else:
+      messagebox.showerror(title="Not Found",message="website not found in file.")
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -53,14 +81,16 @@ email_label.grid(row=2,column=0)
 password_label=Label(text="Password:")
 password_label.grid(row=3,column=0)
 #creating inputs fiels
-website_input=Entry(width=42)
-website_input.grid(row=1,column=1,columnspan=2)
+website_input=Entry(width=32)
+website_input.grid(row=1,column=1)
 website_input.focus()
 email_input=Entry(width=42)
 email_input.grid(row=2,column=1,columnspan=2)
 password_input=Entry(width=32)
 password_input.grid(row=3,column=1)
 #creating buttons
+search_button=Button(text="search",width=6,command=find_password)
+search_button.grid(row=1,column=2)
 generate_password=Button(text="Generate",command=generate_password)
 generate_password.grid(row=3,column=2)
 add_button=Button(text="Save",width=36,command=add)
